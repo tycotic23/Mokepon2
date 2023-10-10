d482f608ebe9250ecd55305c5c58769676161738
tommy stevan <magui.maggie@gmail.com>
2023-10-09 22:16:35 -0300

    initial commit

---
+++ src/main/java/com/mokepon/mokepon/MokeponApplication.java
@@ -0,0 +1,13 @@
+package com.mokepon.mokepon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MokeponApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MokeponApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mokepon/mokepon/MokeponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mokepon.mokepon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MokeponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
