4b8f032cd2c406bd9b0ef34b63b097a3ff6f8649
tommy stevan <magui.maggie@gmail.com>
2023-10-11 01:08:18 -0300

    cookie monster model add

---
+++ src/main/java/com/mokepon/mokepon/models/Cookie.java
@@ -0,0 +1,99 @@
+package com.mokepon.mokepon.models;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Cookie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native",strategy = "native")
+    private long id;
+
+    private String animIdle, animMove;
+    private String name, description;
+   @ElementCollection
+   @Column(name = "animAttack")
+    private List<String> animAttacks=new ArrayList<>();
+    @ElementCollection
+    @Column(name = "attack")
+    private List<String> attacks=new ArrayList<>();
+    private int health;
+
+    public Cookie() {
+    }
+
+    public Cookie(String animIdle, String animMove, String name, String description, List<String> animAttacks, List<String> attacks, int health) {
+        this.animIdle = animIdle;
+        this.animMove = animMove;
+        this.name = name;
+        this.description = description;
+        this.animAttacks = animAttacks;
+        this.attacks = attacks;
+        this.health = health;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getAnimIdle() {
+        return animIdle;
+    }
+
+    public void setAnimIdle(String animIdle) {
+        this.animIdle = animIdle;
+    }
+
+    public String getAnimMove() {
+        return animMove;
+    }
+
+    public void setAnimMove(String animMove) {
+        this.animMove = animMove;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getAnimAttacks() {
+        return animAttacks;
+    }
+
+    public void setAnimAttacks(List<String> animAttacks) {
+        this.animAttacks = animAttacks;
+    }
+
+    public List<String> getAttacks() {
+        return attacks;
+    }
+
+    public void setAttacks(List<String> attacks) {
+        this.attacks = attacks;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+}
